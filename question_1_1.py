a9efc311cdfc5f13ae2d0acdde8d871d18a9437e
Aung Swan Htet Paing <135700688+aungswanhtetpaing@users.noreply.github.com>
2023-07-26 12:09:13 +0630

    Update question_1_1.py

--- question_1_1.py
+++ question_1_1.py
@@ -1,1 +1,15 @@
+import os
+
+def count_csv_files(folder_path):
+    csv_count = 0
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith('.csv'):
+                csv_count += 1
+    return csv_count
+
+if __name__ == "__main__":
+    folder_path = "gw_test/Question1/Dataset"
+    csv_files_count = count_csv_files(gw_test)
+    print(f"CSV File Count: {csv_files_count} files")
 
